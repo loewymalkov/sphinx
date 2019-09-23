9b8b79556fbab1e5540c0f2299b856934e2b4c26
Student <example@example.com>
2019-09-23 13:32:31 -0700

    randomizer works, list is just small

--- Sphinx.cs
+++ Sphinx.cs
@@ -7,23 +7,29 @@ class Sphinx
   {
     Console.WriteLine("Here is a riddle from the sphinx. Answer it correctly or u dead:");
 
-    Dictionary<string, string> riddles = new Dictionary<string, string>() { {"Q1", "A1"}, {"Q2", "A2"} };
+    Dictionary<string, string> riddles = new Dictionary<string, string>() { {"Q1", "A1"}, {"Q2", "A2"}, {"Q3", "A3"} };
 
     Random randy = new Random();
 
-    List<string> riddleList = new List<string>(riddles.Keys)[randy.Next(2)];
+    List<string> riddleList = new List<string>(riddles.Keys);
 
-
-    // List<string> randomRiddles = new List<string>(riddleList[randy.Next(2)]);
-
-    foreach(List<string> entry in riddleList)
+    foreach(var entry in riddleList)
     {
-      Console.WriteLine(entry);
+      string randomRiddle = riddleList[randy.Next(riddleList.Count)];
+      Console.WriteLine(randomRiddle);
       string answer = Console.ReadLine();
-      if ((entry == "Q1") && (answer == "A1")) 
+      if ((randomRiddle == "Q1") && (answer == "A1")) 
       {
         Console.WriteLine("nice job. u did good");
-      } else 
+      } else if ((randomRiddle == "Q2") && (answer == "A2"))
+      {
+        Console.WriteLine("nice job. u did good");
+      }
+      else if ((randomRiddle == "Q3") && (answer == "A3"))
+      {
+        Console.WriteLine("nice job. u pass");
+      }
+      else
       {
         Console.WriteLine("you've been eaten");
         break;
